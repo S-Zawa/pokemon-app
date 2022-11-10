76a12768de3f9e467f54caf5683fe8a0bf5e6605
S.Aizawa <showta4696@gmail.com>
2022-11-10 23:02:50 +0900

    refs #7 ポケモンの詳細データを取得する

--- src/utils/pokemon.ts
+++ src/utils/pokemon.ts
@@ -1,10 +1,22 @@
 import { rejects } from "assert";
 import { resolve } from "path";
+import { AllPokemon } from "../types/AllPokemon";
+import { PokemonDetail } from "../types/PokemonDetail";
 
 export const getAllPokemon = (url: RequestInfo | URL) => {
-  return new Promise((resolve, rejects) => {
+  return new Promise<AllPokemon>((resolve, rejects) => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => resolve(data));
+  });
+};
+
+export const getPokemon = (url: string) => {
+  return new Promise<PokemonDetail>((resolve, rejects) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
   });
 };
