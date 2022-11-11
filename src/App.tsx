79d9f62f783969bab31a11eaa3c5270a007a11ef
S.Aizawa <showta4696@gmail.com>
2022-11-11 23:57:29 +0900

    refs #19 ページングを実装する

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 import Navbar from './components/Navbar/Navbar'
-import { Result } from './types/AllPokemon'
+import { AllPokemon, Result } from './types/AllPokemon'
 import { PokemonDetail } from './types/PokemonDetail'
 import { getAllPokemon, getPokemon } from './utils/pokemon'
 
@@ -10,14 +10,18 @@ function App() {
   const initialUrl = 'https://pokeapi.co/api/v2/pokemon'
   const [loading, setLoading] = useState(true)
   const [pokemonData, setPokemonData] = useState<PokemonDetail[]>([])
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("")
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // すべてのポケモンデータを取得
       const response = await getAllPokemon(initialUrl)
 
-      //各ポケモンの詳細データを取得
+      // 各ポケモンの詳細データを取得
       loadPokemon(response.results)
+      setNextUrl(response.next)
+      setPrevUrl(response.previous)
       setLoading(false)
     }
     fetchPokemonData()
@@ -33,6 +37,27 @@ function App() {
     setPokemonData(_pokemonData)
   }
 
+  const handlePrevPage = async () => {
+    if (!prevUrl) {
+      return;
+    }
+    setLoading(true)
+    const data = await getAllPokemon(prevUrl);
+    await loadPokemon(data.results)
+    setUrl(data)
+    setLoading(false);
+  }
+  const handleNextPage = async () => {
+    setLoading(true);
+    const data = await getAllPokemon(nextUrl);
+    await loadPokemon(data.results)
+    setUrl(data)
+    setLoading(false);
+  }
+  const setUrl = (allPokemon: AllPokemon) => {
+    setPrevUrl(allPokemon.previous)
+    setNextUrl(allPokemon.next);
+  }
   return (
     <>
       <Navbar></Navbar>
@@ -45,6 +70,14 @@ function App() {
               {pokemonData.map((pokemonDetail, i) => {
                 return <Card key={i} pokemonDetail={pokemonDetail}></Card>
               })}
+            </div>
+            <div className='btn'>
+              {!prevUrl ?
+                (<></>) : <button onClick={handlePrevPage}>前へ</button>
+              }
+              {!nextUrl ?
+                (<></>) : <button onClick={handleNextPage}>次へ</button>
+              }
             </div>
           </>
         )}
